4590866378035ae5015322437c237e2e70b9e84e
payrollpro-backend <contracting@goodfellasholdings.com>
2025-12-11 18:24:49 -0500

    Create generateToken.js

---
+++ src/utils/generateToken.js
@@ -0,0 +1,16 @@
+// utils/generateToken.js
+const jwt = require('jsonwebtoken');
+
+function generateToken(admin) {
+  return jwt.sign(
+    {
+      id: admin._id,
+      email: admin.email,
+      role: admin.role,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: '7d' }
+  );
+}
+
+module.exports = generateToken;
